6bfa99299453390eaefbb0b78828abc1ac7e81dd
tbriggs1 <tombriggs93@protonmail.com>
2021-02-27 19:03:53 +0000

    final changes

--- src/components/Player.js
+++ src/components/Player.js
@@ -73,7 +73,7 @@ const Player = ({ currentSong, isPlaying, setIsPlaying, audioRef, setSongInfo, s
             <div className="time-control">
                 <p>{getTime(songInfo.currentTime)}</p>
                 <input min={0} max={songInfo.duration || 0} value={songInfo.currentTime} onChange={dragHandler} type="range" />
-                <p>{getTime(songInfo.duration)}</p>
+                <p>{songInfo.duration ? getTime(songInfo.duration) : "0:00"}</p>
             </div>
             <div className="play-control">
                 <FontAwesomeIcon className="skip-back" size="2x" icon={faAngleLeft} onClick={() => skipTractHandler('skip-back')}/>
