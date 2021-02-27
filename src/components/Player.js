9b5d51d49677641ab24efcf5d4d30f0f11373c59
tbriggs1 <tombriggs93@protonmail.com>
2021-02-27 10:14:10 +0000

    Updated the progress bar

--- src/components/Player.js
+++ src/components/Player.js
@@ -13,12 +13,15 @@ const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
         if(isPlaying){
             audioRef.current.pause();
             setIsPlaying(!isPlaying);
-            setPButton(faPlay);
         }else{
             audioRef.current.play();
             setIsPlaying(!isPlaying);
-            setPButton(faPause);
         }
+    }
+
+    const dragHandler = (e) => {
+        audioRef.current.currentTime = e.target.value;
+        setSongInfo({...songInfo, currentTime: e.target.value});
     }
 
     const timeUpdateHander = (e) => {
@@ -34,22 +37,22 @@ const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
     }
     //State
     const [songInfo, setSongInfo] = useState({
-        currentTime: null,
-        duration: null,
+        currentTime: 0,
+        duration: 0,
     });
     
-    const [pButton, setPButton] = useState(faPlay);
+
 
     return (
         <div className="player">
             <div className="time-control">
                 <p>{getTime(songInfo.currentTime)}</p>
-                <input type="range" />
+                <input min={0} max={songInfo.duration} value={songInfo.currentTime} onChange={dragHandler} type="range" />
                 <p>{getTime(songInfo.duration)}</p>
             </div>
             <div className="play-control">
                 <FontAwesomeIcon className="skip-back" size="2x" icon={faAngleLeft}/>
-                <FontAwesomeIcon onClick={playSongHandler} className="play" size="2x" icon={pButton}/>
+                <FontAwesomeIcon onClick={playSongHandler} className="play" size="2x" icon={!isPlaying ? faPlay : faPause}/>
                 <FontAwesomeIcon className="skip-forward" size="2x" icon={faAngleRight}/>
             </div>
             <audio onTimeUpdate={timeUpdateHander} onLoadMetadata={timeUpdateHander} ref={audioRef} src={currentSong.audio}></audio>
